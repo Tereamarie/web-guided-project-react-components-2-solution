90686131246e6518b89148ce47e48efc725eb22c
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-12 15:01:56 +0200

    comments App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,23 @@
 // This is the top-level component
 // so we'll keep application state at this level.
-// Import the state hook!
+// 1- Import the state hook!
 import React, { useState } from 'react'
 import FriendsList from './FriendsList'
 import Search from './Search'
-// Import the dummy data that will power the application.
-// Tomorrow we'll fetch the data from an API instead.
+// 2- Import the dummy data that will power the application.
+// (Tomorrow we'll fetch the data from an API instead.)
 import friendsData from '../dummy-data/friends'
 
 
 export default function App() {
-  // Initialize a slice of state to keep track of the data
+  // 3- Initialize a slice of state to keep track of the data
   // using the dummy data as the initial value of the slice of state
   const [friends, setFriends] = useState(friendsData)
-  // Initialize a slice to keep track of the value of the search box
+  // 4- Initialize a slice to keep track of the value of the search box
   // using an empty string as the initial value of the slice
   const [searchTerm, setSearchTerm] = useState('')
-  // STRETCH - Make a helper function that
-  // returns a filtered array of friends data (filtering by search term)
+  // STRETCH - Make a helper function that returns
+  // a filtered array of friends data (filtering by search term)
   const getFilteredFriends = () => {
     const term = searchTerm.trim()
     return friends.filter(friend => {
@@ -32,11 +32,11 @@ export default function App() {
 
   return (
     <div className='app-friends container'>
-      {/* Render the Search component */}
+      {/* 5- Render the Search component */}
       {/* STRETCH - Changes to the input should update the search term */}
       <Search setSearchTerm={setSearchTerm} />
 
-      {/* Render the FriendsList component */}
+      {/* 6- Render the FriendsList component */}
       {/* What prop/props does FriendsList need? */}
       <FriendsList friends={getFilteredFriends()}/>
     </div>
